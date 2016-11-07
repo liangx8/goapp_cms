4cb6c9b2b312ac3fc904eb47f8b57bfe548da53d
wang.office.xin <wang.office.gang@gmail.com>
2016-11-07 10:39:59 +0800

    list content of account

--- pfa/dao.go
+++ pfa/dao.go
@@ -6,34 +6,49 @@ import (
 	"cloud.google.com/go/storage"
 	"google.golang.org/appengine/log"
 
+	"io/ioutil"
 )
 
 type Dao struct{
-	ctx context.Context
 	client *storage.Client
-	b *storage.BucketHandle
-	account string
+	Account func() string
+	Save func([]Expense) error
+	Load func(*[]Expense) error
 }
 func NewDao(ctx context.Context,a string) (*Dao,error){
 	cli,err := storage.NewClient(ctx)
 	if err != nil { return nil,err }
 	bucket := cli.Bucket(bucketName)
-	return &Dao{ctx:ctx,client:cli,account:a,b:bucket},nil
+	fn := prefix + a + ".yaml"
+	return &Dao{
+		client  :cli,
+		Account :func()string{
+			return a
+		},
+		Save    : func(es []Expense)error{
+			buf,err := yaml.Marshal(es)
+			if err != nil { return err }
+			log.Errorf(ctx,fn)
+			oh := bucket.Object(fn)
+			w:=oh.NewWriter(ctx)
+			_,err = w.Write(buf)
+			if err != nil { return err }
+			err=w.Close()
+			if err != nil { return err }
+			return nil
+		},
+		Load    :func(es *[]Expense) error{
+			oh := bucket.Object(fn)
+			objr,err := oh.NewReader(ctx)
+			if err != nil { return err }
+			buf,err :=ioutil.ReadAll(objr)
+			if err != nil { return err }
+			err = yaml.Unmarshal(buf,es)
+			return err
+		},
+	},nil
 }
 
-func (dao *Dao)Save(es []Expense)error{
-	b,err := yaml.Marshal(es)
-	if err != nil { return err }
-	fn := prefix + dao.account + ".yaml"
-	log.Errorf(dao.ctx,fn)
-	oh := dao.b.Object(fn)
-	w:=oh.NewWriter(dao.ctx)
-	_,err = w.Write(b)
-	if err != nil { return err }
-	err=w.Close()
-	if err != nil { return err }
-	return nil
-}
 func (dao *Dao)Close()error{
 	return dao.client.Close()
 }
--- pfa/index.go
+++ pfa/index.go
@@ -16,6 +16,22 @@ import (
 	"github.com/liangx8/gcloud-helper/gcs"
 )
 
+type ErrorResult struct{
+	Title,Head,Message string
+	Err error
+}
+
+func DefaultErrorResut(title string,err error) ErrorResult{
+	return ErrorResult{
+		Title:title,
+		Head:title,
+		Message:func() string{if err==nil{return ""} else {return err.Error()}}(),
+		Err:err,
+	}
+}
+
+var tempError = template.Must(template.New("").Parse(error_tmpl))
+
 func index(ctx context.Context){
 	bucket,err :=gcs.NewBucket(ctx,ProjectId,"pfa.rc-greed.com")
 	if err != nil {
@@ -30,13 +46,18 @@ func index(ctx context.Context){
 		}
 		account := make([]string,0,10)
 		e:=bucket.Objects(gcs.AttrCallback(func(attrs *storage.ObjectAttrs) error{
-			account = append(account,attrs.Name[8:])
+			aName := attrs.Name[8:]
+			length := len(aName)
+			account = append(account,aName[:length-5])
 			return nil
 		}),&q)
 		if e!= nil {
 			log.Errorf(ctx,"%v",err)
 		}
-		tmpl := buildTemplate(ctx,bucket,"tmpl/account_list.tmpl",account_list_tmpl)
+		tmpl,er := buildTemplate(ctx,bucket,"tmpl/account_list.tmpl")
+		if er != nil {
+			tmpl = template.Must(template.New("").Parse(account_list_tmpl))
+		}
 		w.Header().Add("Content-Type","text/html;charset=UTF-8")
 		tmpl.Execute(w,account)
 	})
@@ -53,7 +74,30 @@ func Account(ctx context.Context){
 	}
 	defer bucket.Close()
 	err = spark.HttpHandleFunc(ctx,func(w http.ResponseWriter, r *http.Request){
-		buildTemplate(ctx,bucket,"tmpl/account.tmpl",no_tmpl).Execute(w,nil)
+		account:=r.FormValue("name")
+		if account == ""{
+			tempError.Execute(w,DefaultErrorResut("页面没有找到",nil))
+			return
+		}
+		es := make([]Expense,0,50)
+		dao,err := NewDao(ctx,account)
+		if err == nil {
+			err=dao.Load(&es)
+		}
+		if err != nil {
+			tempError.Execute(w,DefaultErrorResut("出错"+account,err))
+			return
+		}
+		defer dao.Close()
+		tmpl,er:=buildTemplate(ctx,bucket,"tmpl/account.tmpl")
+		if er != nil {
+			tempError.Execute(w,er)
+			return
+		}
+		model :=make(map[string]interface{})
+		model["accountName"]="虚拟帐套";
+		model["data"]=es
+		tmpl.Execute(w,model)
 	})
 	if err != nil {
 		log.Errorf(ctx,"%v",err)
@@ -61,18 +105,28 @@ func Account(ctx context.Context){
 	}
 
 }
-func buildTemplate(ctx context.Context,bkt *gcs.Bucket,name,defaultStr string) *template.Template{
+func buildTemplate(ctx context.Context,bkt *gcs.Bucket,name string) (*template.Template,*ErrorResult){
 //		tmplObj:=bkt.Object("tmpl/account_list.tmpl")
 	tmplObj:=bkt.Object(name)
 	objr,err := tmplObj.NewReader(ctx)
 	if err == storage.ErrObjectNotExist {
-		return template.Must(template.New("").Parse(defaultStr))
+		return nil, &ErrorResult{
+			Title   :"找不到模板",
+			Head    :"你必须定义一个模板",
+			Message :"必须定义一个模板"+name+"以便于显示内容",
+			Err     :err,
+		}
 	} else {
 		if buf,err := ioutil.ReadAll(objr); err == nil {
 			tmpl, err := template.New("").Parse(string(buf))
-			if err == nil { return tmpl }
+			if err == nil { return tmpl,nil }
 		}
-		return template.Must(template.New("").Parse(defaultStr))
+		return nil, &ErrorResult{
+			Title   :"模板错误",
+			Head    :"模板错误",
+			Message :"读取模板时错误",
+			Err     :err,
+		}
 	}
 	panic("never reach here")
 }
@@ -91,5 +145,37 @@ const (
 </ul>
 </body>
 </html>`
-	no_tmpl = "<html><head><title>你必须定义一个模板</title></head><body>必须定义一个模板以便于显示内容</body></html>"
+	no_tmpl = "<html><head><title>你必须定义一个模板</title></head><body>必须定义一个模板'tmpl/account_list.tmpl'以便于显示内容</body></html>"
+	error_tmpl=`<html>
+<head><title>Error: {{.Title}}</title>
+<style type="text/css">
+html, body {
+	font-family: "Roboto", sans-serif;
+	color: #333333;
+	background-color: #ea5343;
+	margin: 0px;
+}
+h1 {
+	color: #d04526;
+	background-color: #ffffff;
+	padding: 20px;
+	border-bottom: 1px dashed #2b3848;
+}
+pre {
+	margin: 20px;
+	padding: 20px;
+	border: 2px solid #2b3848;
+	background-color: #ffffff;
+}
+</style>
+</head><body>
+<h1>Error</h1>
+<pre style="font-weight: bold;">{{.Head}}</pre>
+<pre>{{.Message}}</pre>
+{{if .Err}}
+<pre>{{.Err}}</pre>
+{{end}}
+</body>
+</html>`
+
 )
