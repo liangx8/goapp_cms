77d0d8c7f9e31a91db796180e7268801ddc5a7c5
wang.office.xin <wang.office.gang@gmail.com>
2017-02-13 11:50:18 +0800

    new version

--- web/webmain.go
+++ web/webmain.go
@@ -2,19 +2,21 @@ package web
 
 import (
 	"fmt"
-	"sync"
+//	"sync"
 
 	"golang.org/x/net/context"
 	"google.golang.org/appengine"
 	"google.golang.org/appengine/log"
+	"google.golang.org/appengine/file"
 	
 	"github.com/liangx8/gcloud-helper/gcs"
 //	"github.com/liangx8/spark/session"
 	"github.com/liangx8/spark"
 
-	"pfa"
+//	"pfa"
 	"cloud.google.com/go/storage"
 )
+
 
 func index(ctx context.Context){
 	w,r,err:=spark.ReadHttpContext(ctx)
@@ -23,18 +25,26 @@ func index(ctx context.Context){
 		return
 	}
 	
-	bucket,err := gcs.NewBucket(ctx,pfa.ProjectId,"pfa.rc-greed.com")
+	bucket,err := gcs.NewBucket(ctx,projectId,bucketName)
+	if err != nil {
+		fmt.Fprintln(w,err)
+		log.Errorf(ctx,"%v",err)
+		return
+	}
+	defer bucket.Close()
+	x,err := file.DefaultBucketName(ctx)
 	if err != nil {
 		fmt.Fprintln(w,err)
 		return
 	}
-	defer bucket.Close()
+	fmt.Fprintln(w,x)
 	err = bucket.Objects(gcs.AttrCallback(func(attrs *storage.ObjectAttrs)error{
 		fmt.Fprintln(w,attrs.Name)
 		return nil
 	}),nil)
 	if err != nil {
-		fmt.Fprint(w,err)
+		fmt.Fprintf(w,"read objects error:%v",err)
+		log.Errorf(ctx,"%v",err)
 		return
 	}
 	fmt.Fprintf(w,"链接:%s\n",r.URL.RequestURI())
@@ -42,8 +52,10 @@ func index(ctx context.Context){
 
 func init(){
 	spk:=spark.New(appengine.NewContext)
-	spk.HandleFunc("/pfa/account",pfa.Account)
-	spk.HandleFunc("/pfa/",pfa.PFA)
 	spk.HandleFunc("/",index)
 }
-var once sync.Once
+
+const (
+	projectId= "personal-financial-140007"
+	bucketName="pfa.rc-greed.com"
+)
